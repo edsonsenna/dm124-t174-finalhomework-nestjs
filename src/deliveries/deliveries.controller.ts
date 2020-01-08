dbb5b92d56ab790a1842fe576a1b1250eb84e0e8
Edson de Senna Junior <edsondesennajunior@MacBook-Air-de-Edson.local>
2020-01-07 21:35:18 -0300

    Setting swagger

--- src/deliveries/deliveries.controller.ts
+++ src/deliveries/deliveries.controller.ts
@@ -3,7 +3,10 @@ import { Controller, Get, Param, Post, Body, Patch, Delete, UseGuards } from '@n
 import { Delivery } from './delivery';
 import { DeliveriesService } from './deliveries.service';
 import { AuthGuard } from '@nestjs/passport';
+import { ApiBody, ApiCreatedResponse, ApiBearerAuth, ApiOkResponse, ApiTags, ApiParam } from '@nestjs/swagger';
+import { DeliveryDto } from './delivery-dto';
 
+@ApiTags('deliveries')
 @Controller('deliveries')
 export class DeliveriesController {
 
@@ -12,29 +15,69 @@ export class DeliveriesController {
     ) {}
 
     @Get('/')
+    @ApiOkResponse({
+        description: 'The records has been successfully loaded',
+        type: [DeliveryDto]
+    })
     async findAll(): Promise<Array<Delivery>> {
         return await this.deliveriesService.findAll();
     }
 
     @Get('/:id')
+    @ApiParam({
+        name: 'id',
+        type: 'string',
+        description: 'A delivery identifier'
+    })
+    @ApiOkResponse({
+        description: 'The record has been successfully found.',
+        type: DeliveryDto,
+    })
     async findById(@Param() params): Promise<Delivery>{
         return await this.deliveriesService.findById(params.id);
     }
 
     @UseGuards(AuthGuard('jwt'))
     @Post('/')
+    @ApiBearerAuth()
+    @ApiBody({ type: DeliveryDto })
+    @ApiCreatedResponse({
+        description: 'The record has been successfully created.',
+        type: DeliveryDto
+    })
     async create(@Body() delivery: Delivery): Promise<Delivery> {
         return await this.deliveriesService.create(delivery);
     }
 
     @UseGuards(AuthGuard('jwt'))
+    @ApiBearerAuth()
     @Patch('/:id')
+    @ApiBody({ type: DeliveryDto })
+    @ApiParam({
+        name: 'id',
+        type: 'string',
+        description: 'A delivery identifier'
+    })
+    @ApiCreatedResponse({
+        description: 'The record has been successfully updated.',
+        type: DeliveryDto
+    })
     async update(@Param() params, @Body() delivery: Delivery): Promise<Delivery> {
         return await this.deliveriesService.update(params.id, delivery);
     }
 
     @UseGuards(AuthGuard('jwt'))
+    @ApiBearerAuth()
     @Delete('/:id')
+    @ApiParam({
+        name: 'id',
+        type: 'string',
+        description: 'A delivery identifier'
+    })
+    @ApiCreatedResponse({
+        description: 'The record has been successfully deleted.',
+        type: DeliveryDto
+    })
     async delete(@Param() params): Promise<boolean> {
         return await this.deliveriesService.delete(params.id);
     }
